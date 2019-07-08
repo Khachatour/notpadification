765cc93c7d1f6def1f43053356108413b761dbc0
Khachatour <loledgamer96@hotmail.com>
2019-07-08 18:53:30 +0400

    core: add validation and add logic

--- src/lib/components/InitialNote/InitialNote.js
+++ src/lib/components/InitialNote/InitialNote.js
@@ -3,13 +3,41 @@ import React, { useState } from 'react'
 import Text from '../Text'
 import Input from '../Input'
 import Button from '../Button'
+import If from '../Utils/Conditional'
 import './InitialNote.styles.sass'
+const MAX_TITLE_COUNT = 255
+const MAX_NOTE_COUNT = 1000
+const initState = { title: null, note: null }
 
 const InitialNote = () => {
   const [title, setTitle] = useState('')
   const [note, setNote] = useState('')
-
+  const [warning, setWarning] = useState(initState)
   const onAdd = () => {}
+
+  const checkAndUpdate = (
+    text: string,
+    prop: string,
+    maxCount: number,
+    setter: (s: string) => void
+  ) => {
+    const text = e.target.value
+    if (text.length < MAX_TITLE_COUNT) {
+      setter(text)
+      setWarning(initState)
+    } else {
+      setWarning({
+        ...warning,
+        [prop]: `Sorry, you can only type ${MAX_TITLE_COUNT} number of characters`
+      })
+    }
+  }
+
+  const onChangeTitle = (e: Event) =>
+    checkAndUpdate(e.target.value, 'title', MAX_TITLE_COUNT, setTitle)
+
+  const onChangeNote = (e: Event) =>
+    checkAndUpdate(e.target.value, 'note', MAX_NOTE_COUNT, setNote)
 
   return (
     <div className="initial-note">
@@ -17,10 +45,14 @@ const InitialNote = () => {
       <Input
         value={title}
         isUnique={true}
-        onChange={setTitle}
+        onChange={onChangeTitle}
         placeholder="enter note title"
       />
-      <textarea value={note} onChange={setNote} placeholder="enter note" />
+      <If
+        condition={!!warning.title}
+        then={() => <Text>{warning.title}</Text>}
+      />
+      <textarea value={note} onChange={onChangeNote} placeholder="enter note" />
       <Button type="success" onClick={onAdd}>
         Add
       </Button>
