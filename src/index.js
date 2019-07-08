720eff46b0a25907647d33c3b21cc9690dbaf5a0
Khachatour <loledgamer96@hotmail.com>
2019-07-08 14:40:02 +0400

    app: create initial app structure with HMR and parcel
     - add sass prep
     - add prettier and eslint
     - add `demo` for dev env
     - install react react-dom

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.sass'
+
+const Hello = () => <h1 className="test-class">Hello</h1>
+
+ReactDOM.render(<Hello />, document.getElementById('root'))
